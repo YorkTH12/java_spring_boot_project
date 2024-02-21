f856ea785c467834dd5719618e55e85840304e06
YorkTH12 <148939113+YorkTH12@users.noreply.github.com>
2024-02-22 03:37:20 +0700

    Delete java_spring_boot_project directory

--- java_spring_boot_project/src/main/java/com/project/project/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.project.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, String>{
-
-}
